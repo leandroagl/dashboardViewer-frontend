eb0b7824a5a774e956522ac9a8aa58c8afdedb41
unknown <leandro.n.aguilera@gmail.com>
2026-02-28 01:20:12 -0300

    feat(admin): cargar clientes activos en AdminLayoutComponent

--- src/app/modules/admin/layout/admin-layout.component.ts
+++ src/app/modules/admin/layout/admin-layout.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
+import { ClientsService } from '@core/services/clients.service';
+import { Client } from '@core/models';
 
 @Component({
   selector: 'app-admin-layout',
@@ -8,9 +10,18 @@ import { AuthService } from '@core/services/auth.service';
   templateUrl: './admin-layout.component.html',
   styleUrls:   ['./admin-layout.component.scss'],
 })
-export class AdminLayoutComponent {
-  private readonly auth = inject(AuthService);
-  protected readonly clienteSlug = this.auth.clienteSlug;
+export class AdminLayoutComponent implements OnInit {
+  private readonly auth    = inject(AuthService);
+  private readonly clients = inject(ClientsService);
+
+  protected readonly clienteSlug   = this.auth.clienteSlug;
+  protected readonly activeClients = signal<Client[]>([]);
+
+  ngOnInit(): void {
+    this.clients.getAll().subscribe(list =>
+      this.activeClients.set(list.filter(c => c.activo))
+    );
+  }
 
   protected logout(): void { this.auth.logout(); }
 }
